2a661b16ac9dceb8496483acdbf7bd4d2fe3121c
Hemanth Kumar <avisihks@gmail.com>
2023-10-26 09:10:11 +0530

    Add Client Auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -8,60 +8,88 @@ import ChatDetailedPage from "./pages/ChatDetailedPage";
 import CreateNewGroupPage from "./pages/CreateNewGroupPage";
 import Cookies from "js-cookie";
 
+const AuthRoute = ({ element, authenticated }) => {
+  return authenticated ? element : <Navigate to="/" />;
+};
+
 const App = () => {
   const [groups, setGroups] = useState([]);
   const cookie = Cookies.get("userInfo");
+  let authenticated = cookie !== undefined;
   let userData = {};
-  if (cookie !== undefined) {
+  if (cookie) {
     userData = JSON.parse(cookie);
   }
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={authenticated ? <Navigate to="/home" /> : <LoginPage />}
+        />
         <Route path="/signup" element={<SignUpPage />} />
         <Route
           path="/home"
           element={
-            <HomePage
-              groups={groups}
-              setGroups={setGroups}
-              userId={userData.userId}
-              userName={userData.userName}
+            <AuthRoute
+              authenticated={authenticated}
+              element={
+                <HomePage
+                  groups={groups}
+                  setGroups={setGroups}
+                  userId={userData.userId}
+                  userName={userData.userName}
+                />
+              }
             />
           }
         />
         <Route
           path="/create-new-group"
           element={
-            <CreateNewGroupPage
-              groups={groups}
-              setGroups={setGroups}
-              userId={userData.userId}
-              userName={userData.userName}
+            <AuthRoute
+              authenticated={authenticated}
+              element={
+                <CreateNewGroupPage
+                  groups={groups}
+                  setGroups={setGroups}
+                  userId={userData.userId}
+                  userName={userData.userName}
+                />
+              }
             />
           }
         />
         <Route
           path="/show-invitations"
           element={
-            <ShowInvitationsPage
-              groups={groups}
-              setGroups={setGroups}
-              recieverId={userData.userId}
-              userName={userData.userName}
+            <AuthRoute
+              authenticated={authenticated}
+              element={
+                <ShowInvitationsPage
+                  groups={groups}
+                  setGroups={setGroups}
+                  recieverId={userData.userId}
+                  userName={userData.userName}
+                />
+              }
             />
           }
         />
         <Route
           path="/group-detailed-page/:groupId"
           element={
-            <ChatDetailedPage
-              groups={groups}
-              setGroups={setGroups}
-              userId={userData.userId}
-              userName={userData.userName}
+            <AuthRoute
+              authenticated={authenticated}
+              element={
+                <ChatDetailedPage
+                  groups={groups}
+                  setGroups={setGroups}
+                  userId={userData.userId}
+                  userName={userData.userName}
+                />
+              }
             />
           }
         />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 const Header = ({ userName }) => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
+    Cookies.remove("userInfo");
     navigate("/");
   };
 
